6afb24de50b09f14531cb188707800ae6a505b6e
Yasmin-min <yasminneumann@hotmail.com>
2025-11-24 22:22:52 -0300

    Tests - Ajustes nos testes de login

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,32 +1,32 @@
 import { expect, test } from '@playwright/test';
 import { error } from 'console';
 
-test('Login com sucesso', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
-    await expect(await page.title()).toBe('Swag Labs');
+test.describe('Login testes', async () => {
 
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
+    test('Login com sucesso', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com');
+        await expect(await page.title()).toBe('Swag Labs');
 
-    await page.locator('[data-test="login-button"]').click();
+        await page.locator('[data-test="username"]').fill('standard_user');
+        await page.locator('[data-test="password"]').fill('secret_sauce');
 
-    await expect(await page.url()).toBe('https://www.saucedemo.com/inventory.html');
+        await page.locator('[data-test="login-button"]').click();
 
-    await expect(await page.locator('[data-test="title"]')).toHaveText('Products');
+        await expect(await page.url()).toBe('https://www.saucedemo.com/inventory.html');
 
-    await page.pause();
+        await expect(await page.locator('[data-test="title"]')).toHaveText('Products');
+    })
+
+    test('Usuário inseri credenciais inválidas', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com');
+
+        await page.locator('[data-test="username"]').fill('invalid_user');
+        await page.locator('[data-test="password"]').fill('invalid_password');
+
+        await page.locator('[data-test="login-button"]').click();
+
+        const errorTest = page.getByText('Epic sadface: Username and password do not match any user in this service');
+
+        await expect(errorTest).toBeVisible();
+    });
 })
-
-test('Usuário inseri credenciais inválidas', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
-
-    await page.pause();
-    await page.locator('[data-test="username"]').fill('invalid_user');
-    await page.locator('[data-test="password"]').fill('invalid_password');
-
-    await page.locator('[data-test="login-button"]').click();
-
-    const errorTest = page.getByText('Epic sadface: Username and password do not match any user in this service');  
-
-    await expect(errorTest).toBeVisible();
-});
